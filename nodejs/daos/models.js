72705e2f3ac4ce4974c61ff6f30053432c1c7d35
MrRisoni <vanguelisk@gmail.com>
2021-05-16 13:26:05 +0300

    deleted unused

--- nodejs/daos/models.js
+++ nodejs/daos/models.js
@@ -1,10 +1,9 @@
 var MYSQL_NAME = process.env.SPRING_APP_DB_ECOMMERCE_USR;
 var MYSQL_PASSWD = process.env.SPRING_APP_DB_ECOMMERCE_PASSWD;
 var MYSQL_HOST = process.env.SPRING_APP_DB_ECOMMERCE_HOST;
-var MYSQL_DB= 'mmorpg_db';
+var MYSQL_DB = 'mmorpg_db';
 
 const Sequelize = require('sequelize');
-
 
 const sequelize = new Sequelize(MYSQL_DB, MYSQL_NAME, MYSQL_PASSWD, {
     host: MYSQL_HOST,
@@ -28,142 +27,139 @@ sequelize
     });
 
 
-
 const itemsMdl = sequelize.define('items', {
-    id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'id',
-        autoIncrement: true,
-        primaryKey: true,
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'id',
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: Sequelize.CHAR,
+            field: 'title'
+        }
     },
-    title: {
-        type: Sequelize.CHAR,
-        field: 'title'
+    {
+        timestamps: false,
+        freezeTableName: true
     }
-},
-{
-    timestamps: false,
-    freezeTableName: true
-}
 );
 
 
-
 const charactersMdl = sequelize.define('characters', {
-    id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'id',
-        autoIncrement: true,
-        primaryKey: true,
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'id',
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: Sequelize.CHAR,
+            field: 'name'
+        }
     },
-    title: {
-        type: Sequelize.CHAR,
-        field: 'name'
+    {
+        timestamps: false,
+        freezeTableName: true
     }
-},
-{
-    timestamps: false,
-    freezeTableName: true
-}
 );
 
 
 const auctionHouseListingMdl = sequelize.define('auction_listings', {
-    id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'id',
-        autoIncrement: true,
-        primaryKey: true,
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'id',
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        itemId: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'item_id'
+        },
+        quantity: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'quantity'
+        },
+        startingBidTotal: {
+            type: Sequelize.FLOAT,
+            field: 'starting_bid_total'
+        },
+        startingBidGold: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'starting_bid_g'
+        },
+        startingBidSilver: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'starting_bid_s'
+        },
+        startingBidCopper: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'starting_bid_c'
+        },
+        buyoutTotal: {
+            type: Sequelize.FLOAT,
+            field: 'buyout_total'
+        },
+        buyoutGold: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'buyout_g'
+        },
+        buyoutSilver: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'buyout_s'
+        },
+        buyoutCopper: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'buyout_c'
+        },
+        delisted: {
+            type: Sequelize.BOOLEAN,
+            field: 'delisted'
+        },
+        sold: {
+            type: Sequelize.BOOLEAN,
+            field: 'sold'
+        }
     },
-    itemId: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'item_id'
-    },
-    quantity: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'quantity'
-    },
-    startingBidTotal: {
-        type: Sequelize.FLOAT,
-        field: 'starting_bid_total'
-    },
-    startingBidGold: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'starting_bid_g'
-    },
-    startingBidSilver: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'starting_bid_s'
-    },
-    startingBidCopper: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'starting_bid_c'
-    },
-    buyoutTotal: {
-        type: Sequelize.FLOAT,
-        field: 'buyout_total'
-    },
-    buyoutGold: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'buyout_g'
-    },
-    buyoutSilver: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'buyout_s'
-    },
-    buyoutCopper: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'buyout_c'
-    },
-    delisted : {
-        type: Sequelize.BOOLEAN,
-        field: 'delisted'
-    },
-    sold : {
-        type: Sequelize.BOOLEAN,
-        field: 'sold'
+    {
+        timestamps: false,
+        freezeTableName: true
     }
-},
-{
-    timestamps: false,
-    freezeTableName: true
-}
 );
 
 
-
 const auctionHouseBiddingMdl = sequelize.define('auction_bids_history', {
-    id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'id',
-        autoIncrement: true,
-        primaryKey: true,
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'id',
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        listingId: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'listing_id'
+        },
+        bidTotal: {
+            type: Sequelize.FLOAT,
+            field: 'bid_total'
+        },
+        bidGold: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'bid_g'
+        },
+        bidSilver: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'bid_s'
+        },
+        bidCopper: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            field: 'bid_c'
+        }
     },
-    listingId: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'listing_id'
-    },
-    bidTotal: {
-        type: Sequelize.FLOAT,
-        field: 'bid_total'
-    },
-    bidGold: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'bid_g'
-    },
-    bidSilver: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'bid_s'
-    },
-    bidCopper: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        field: 'bid_c'
+    {
+        timestamps: false,
+        freezeTableName: true
     }
-},
-{
-    timestamps: false,
-    freezeTableName: true
-}
 );
 
 
--- nodejs/routes/orders_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-let router = express.Router()
-
-const orders_controller = require('./../controllers/orders_controller');
-
-router.get('/api/orders/:orderId', orders_controller.getOrderDetails);
-
-module.exports = router;
